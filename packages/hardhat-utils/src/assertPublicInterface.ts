4671440af7c116c2ed2148936705b9364919919a
olivdb <olivdb@protonmail.com>
2022-02-22 10:57:07 +0000

    Refactor assertPublicMethods

--- packages/hardhat-utils/src/assertPublicInterface.ts
+++ packages/hardhat-utils/src/assertPublicInterface.ts
@@ -1,26 +1,40 @@
 import { expect } from 'chai'
+import { FunctionFragment } from 'ethers/lib/utils'
 import { ethers } from 'hardhat'
 
-export async function assertPublicMutableMethods(name: string, expectedPublicMethods: string[]) {
+type FunctionFilter = (f: FunctionFragment) => boolean
+
+export const NON_INSTRUMENTED_FUNCTION_FILTER: FunctionFilter = (f) => !f.format().startsWith('c_0x')
+export const NON_MUTABLE_FUNCTION_FILTER: FunctionFilter = (f) => {
+  return (
+    NON_INSTRUMENTED_FUNCTION_FILTER(f) && // filter out instrumented methods
+    f.stateMutability !== 'nonpayable' &&
+    f.stateMutability !== 'payable'
+  )
+}
+export const MUTABLE_FUNCTION_FILTER: FunctionFilter = (f) => {
+  return (
+    NON_INSTRUMENTED_FUNCTION_FILTER(f) && // filter out instrumented methods
+    (f.stateMutability === 'nonpayable' || f.stateMutability === 'payable')
+  )
+}
+
+export async function assertPublicMethods(
+  name: string,
+  expectedPublicMethods: string[],
+  filter: FunctionFilter = NON_INSTRUMENTED_FUNCTION_FILTER,
+) {
   const contract = await ethers.getContractFactory(name)
-
   const allModifiableFns = Object.values(contract.interface.functions)
-    .filter((f) => {
-      return f.stateMutability === 'nonpayable' || f.stateMutability === 'payable'
-    })
+    .filter(filter)
     .map((f) => f.format())
-
   expect(allModifiableFns.sort()).to.be.deep.eq(expectedPublicMethods.sort())
 }
 
+export async function assertPublicMutableMethods(name: string, expectedPublicMethods: string[]) {
+  await assertPublicMethods(name, expectedPublicMethods, MUTABLE_FUNCTION_FILTER)
+}
+
 export async function assertPublicNotMutableMethods(name: string, expectedPublicMethods: string[]) {
-  const contract = await ethers.getContractFactory(name)
-
-  const allModifiableFns = Object.values(contract.interface.functions)
-    .filter((f) => {
-      return !(f.stateMutability === 'nonpayable' || f.stateMutability === 'payable')
-    })
-    .map((f) => f.format())
-
-  expect(allModifiableFns.sort()).to.be.deep.eq(expectedPublicMethods.sort())
+  await assertPublicMethods(name, expectedPublicMethods, NON_MUTABLE_FUNCTION_FILTER)
 }
